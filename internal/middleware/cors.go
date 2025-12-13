ebb24f8c54cf4e76b4520b99e4f0cc45aaaeae14
vishnujoshi062 <vishnujoshi062@gmail.com>
2025-12-13 20:45:06 +0530

    cors error fixed

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,7 +1,8 @@
 package middleware
 
-import "github.com/go-chi/cors"
-
+import {"github.com/go-chi/cors"
+		"net/http"
+}
 func CorsMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
 		AllowedOrigins: []string{
@@ -35,3 +36,25 @@ func CorsMiddleware() *cors.Cors {
 		MaxAge:           300,
 	})
 }
+
+
+
+func CorsMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
