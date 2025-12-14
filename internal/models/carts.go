aecc0fcd4c0a29c0aacd61f5e62819d51ed3d3fd
shravanmukunda <123108197+shravanmukunda@users.noreply.github.com>
2025-12-15 00:38:07 +0530

    cart logic changed

--- internal/models/carts.go
+++ internal/models/carts.go
@@ -1,29 +1,29 @@
 package models
 
 import (
-	"time"
+    "time"
 
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 )
 
 type Cart struct {
-	ID        uint `gorm:"primaryKey"`
-	UserID    string `gorm:"uniqueIndex;not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+    ID        uint `gorm:"primaryKey"`
+    UserID    *string `gorm:"index"` // Make it nullable with *string for guest carts
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    DeletedAt gorm.DeletedAt `gorm:"index"`
 
-	User      User       `gorm:"foreignKey:UserID"`
-	CartItems []CartItem `gorm:"foreignKey:CartID"`
+    // No User relationship - UserID stores Clerk ID directly
+    CartItems []CartItem `gorm:"foreignKey:CartID"`
 }
 
 type CartItem struct {
-	ID        uint `gorm:"primaryKey"`
-	CartID    uint `gorm:"not null"`
-	VariantID uint `gorm:"not null"`
-	Quantity  int  `gorm:"not null"`
-	AddedAt   time.Time
+    ID        uint `gorm:"primaryKey"`
+    CartID    uint `gorm:"not null"`
+    VariantID uint `gorm:"not null"`
+    Quantity  int  `gorm:"not null"`
+    AddedAt   time.Time
 
-	Cart    Cart           `gorm:"foreignKey:CartID"`
-	Variant ProductVariant `gorm:"foreignKey:VariantID"`
+    Cart    Cart           `gorm:"foreignKey:CartID"`
+    Variant ProductVariant `gorm:"foreignKey:VariantID"`
 }
